d072aa8b7928a963567423541935b466180f2d79
Nityom <nityomtikherr@gmail.com>
2025-06-10 14:46:22 +0530

    chore: add autoprefixer and postcss configuration; remove tailwindcss from vite config; create tailwind config file

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
-import tailwindcss from '@tailwindcss/vite';
 import viteImagemin from 'vite-plugin-imagemin';
 
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss(),
     viteImagemin({
       gifsicle: {
         optimizationLevel: 7,
