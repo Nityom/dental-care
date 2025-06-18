9a560c90df56ed0c983f3e7f0a81f4c3223dbb32
Nityom <nityomtikherr@gmail.com>
2025-06-18 21:12:52 +0530

    fix: update map link in Footer component for accurate location

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import Form from './Form';
 import { FaMapMarkerAlt, FaPhone, FaEnvelope, FaClock } from 'react-icons/fa';
 
 const Footer = () => {
-  const mapLink = 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3664.542067162587!2d77.4617283753475!3d23.25299030843664!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x397c43875c0232c1%3A0x9991260df695fada!2sSmile%20Craft%20Advanced%20Multi-speciality%20Dental%20Clinic!5e0!3m2!1sen!2sin!4v1718000000000';
+const mapLink = 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3665.9511739550746!2d77.437421!3d23.212365!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMjPCsDEyJzQ0LjUiTiA3N8KwMjYnMTQuNyJF!5e0!3m2!1sen!2sin!4v1718700000000';
 
   return (
     <footer className="w-full bg-gradient-to-b from-[#a7aaca] to-[#a7aaca] py-10 px-0 md:px-16 mt-16">
